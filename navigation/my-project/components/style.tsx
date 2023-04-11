3182802470940cc5d925e1c2d35e1b1bd8019d70
MauroPantano <117815692+MauroPantano@users.noreply.github.com>
2023-04-11 09:27:01 +0200

    Add img and style

---
+++ navigation/my-project/components/style.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { StyleSheet } from "react-native";
+const style = StyleSheet.create({
+    container: {
+        alignItems: 'center', 
+        justifyContent: 'center',
+        width: 300,
+        height: 150,
+        marginLeft: 50,
+        marginTop: 30,
+        flexDirection: "row",
+        backgroundColor: '#C4E4FF'
+    },
+    description: {
+        width: 180,
+        height: 150,
+        marginLeft: 15,
+        marginTop: 45,
+        fontSize: 20
+    }
+})
+
+export default style
